bd822d2a672d3330115abcb92facd6f82a8bfa27
saikrishna446 <66399465+saikrishna446@users.noreply.github.com>
2024-03-04 06:36:13 +0530

    Update Testing33.java
    testing

--- src/test/java/Java/Testing33.java
+++ src/test/java/Java/Testing33.java
@@ -4,7 +4,7 @@ public class Testing33 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("saikrishna");
+		System.out.println("Testing sai");
 
 	}
 
